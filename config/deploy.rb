9cb01658c56303856925c06fa0f595926448ca9a
Ivan Shamatov <status.enable@gmail.com>
2014-01-17 10:50:50 +0400

    capistraning

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+require 'bundler/capistrano'
+
+set :application, "iconizer"
+set :repository,  "git@github.com:IvanShamatov/iconizer.git"
+set :scm, :git
+default_run_options[:pty] = true
+
+set :user, "deploy"
+
+role :web, "ikonkin.ru"                          # Your HTTP server, Apache/etc
+role :app, "ikonkin.ru"                          # This may be the same as your `Web` server
+
+
+set :deploy_to, "/home/deploy/ikonkin"
+
+namespace :deploy do
+ 
+  task :restart do
+    run "cd #{current_path} && thin restart -C config/thin.yml"
+  end
+ 
+  task :start do
+    run "cd #{current_path} && thin start -C config/thin.yml"
+  end
+ 
+  task :stop do
+    run "cd #{current_path} && thin stop -C config/thin.yml"
+  end
+ 
+end
