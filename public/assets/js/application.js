c96cb0e6079fe8d84543d7666dc5edf576988ca0
Anton Shishkin <shishkin@tprs.ru>
2014-01-22 17:41:52 +0400

    вытащил ассетсы из лейаута. дропать файлы можно только в серую штуку

---
+++ public/assets/js/application.js
@@ -0,0 +1,57 @@
+// fileupload staff
+$(function () {
+  // disable dropping to a whole page
+  $(document).bind('drop dragover', function (e) {
+    e.preventDefault();
+  });
+
+  $(document).bind('dragover', function (e) {
+    var dropZone = $('#dropzone'),
+    timeout = window.dropZoneTimeout; // мерцает из-за маленького значения?
+    if (!timeout) {
+      dropZone.addClass('in');
+    } else {
+      // clearTimeout(timeout);
+    }
+    var found = false,
+      node = e.target;
+    do {
+      if (node === dropZone[0]) {
+        found = true;
+        break;
+      }
+      node = node.parentNode;
+    } while (node != null);
+    if (found) {
+      dropZone.addClass('hover');
+    } else {
+      dropZone.removeClass('hover');
+    }
+    window.dropZoneTimeout = setTimeout(function () {
+      window.dropZoneTimeout = null;
+      dropZone.removeClass('in hover');
+    }, 100);
+  });
+
+
+  $('#fileupload').fileupload({
+    dataType: 'json',
+    dropZone: $('#dropzone'),
+    done: function (e, data) {
+      $.each(data.result.icon, function (index, file) {
+        var link = "<a href='/download' target='_blank'>download</a>";
+        // $('#progress').fadeOut();
+        $('#result').html(link).hide().fadeIn();
+
+      });
+    },
+    progressall: function (e, data) {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      $('#progress .progress-bar').css(
+        'width',
+        progress + '%'
+      );
+    }
+  });
+});
+
